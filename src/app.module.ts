5a29c0f58e061a8aba74aa9e13c25c44908e5757
Enriko Raymundu <116963958+Enrikokokoko@users.noreply.github.com>
2023-05-11 20:56:19 +0300

    added db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { DB } from './config';
+import { DB_URL } from './config';
 
 @Module({
-  imports: [
-    AuthModule,
-    UserModule,
-    MongooseModule.forRoot(
-      `mongodb+srv://${DB.USERNAME}:${DB.PASSWORD}@cluster0.ybyyajc.mongodb.net/?retryWrites=true&w=majority`,
-    ),
-  ],
+  imports: [AuthModule, UserModule, MongooseModule.forRoot(DB_URL)],
 })
 export class AppModule {}
