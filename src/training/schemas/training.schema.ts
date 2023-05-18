88d275da460470f090295529001756a4d069a7f7
Enriko Raymundu <116963958+Enrikokokoko@users.noreply.github.com>
2023-05-18 18:48:01 +0300

    updated date schema and relocated schema exercise

--- src/training/schemas/training.schema.ts
+++ src/training/schemas/training.schema.ts
@@ -1,13 +1,15 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import mongoose from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
 import { User } from "src/user/schemas/user.schema";
+
+export type TrainingDocument = HydratedDocument<Training, Exercise>
 
 @Schema()
 export class Training {
     @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User'})
     user: User;
 
-    @Prop({ type: Date, startAt: '2023-06-02'})
+    @Prop({ type: Date, default: Date.now})
     date: number;
 
     @Prop({ type: Date })
@@ -15,3 +17,26 @@ export class Training {
 }
 
 export const TrainingSchema = SchemaFactory.createForClass(Training)
+
+@Schema()
+export class Exercise{
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Training'})
+    training: Training;
+
+    @Prop()
+    title: string;
+
+    @Prop()
+    reps: number;
+
+    @Prop()
+    wight: number;
+
+    @Prop()
+    sets: number;
+
+    @Prop()
+    comment: string;
+}
+
+export const exerciseSchema = SchemaFactory.createForClass(Exercise)
